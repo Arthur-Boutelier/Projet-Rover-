ac628e0c3a4c279bfebbeb6533aec7c164a3aa56
Arthur <arthur.boutelier@gmail.com>
2024-10-22 19:04:52 +0200

    modif nom variable

--- list.h
+++ list.h
@@ -1,15 +1,15 @@
 #include "tree.h"
 #ifndef UNTITLED1_LIST_H
 #define UNTITLED1_LIST_H
-typedef struct s_node_tree{
-t_node_n_aire* node;
-struct s_node_tree* next;
-} t_node_tree;
+typedef struct s_cell_tree{
+t_node_n_aire* value;
+struct s_cell_tree* next;
+} t_cell_tree;
 
 typedef struct s_list{
-    t_node_tree* head;
+    t_cell_tree* head;
 }t_list_tree;
 
-t_node_tree* create_node(int val, char* action);
-void insert_node(t_list* list,  int value, char* action);
+t_cell_tree* create_cell(int val, char* action);
+void insert_cell(t_list_tree* list,  int value, char* action);
 #endif //UNTITLED1_LIST_H
--- tree.h
+++ tree.h
@@ -1,10 +1,6 @@
-//
-// Created by flasque on 19/10/2024.
-//
-
 #ifndef UNTITLED1_LOC_H
 #define UNTITLED1_LOC_H
-
+#include "list.h"
 typedef struct s_node_n_aire{
     int value;
     char* action;
