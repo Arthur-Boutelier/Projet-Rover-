0f400bfa74322244cfa52ee7323cd4e4b3c46e28
arthu <arthur.boutelier@gmail.com>
2024-11-12 22:33:31 +0100

    suppression des éléménts obsolètes apres les précisions données dans la V2

--- list.h
+++ list.h
@@ -2,4 +2,15 @@
 #define UNTITLED1_LIST_H
 #include "tree.h"
 
+
+typedef struct s_cell_son{
+    t_node* curr_node;
+    struct s_cell_son * next;
+}t_cell_son;
+
+typedef struct s_list_son
+{
+    t_cell_son* head;
+}t_list_son;
+
 #endif //UNTITLED1_LIST_H
