ec9895557262bd4379b02f1d7542a60cae9a724f
arthu <arthur.boutelier@gmail.com>
2024-11-12 21:59:12 +0100

    suppression des éléménts obsolètes apres les précisions données dans la V2

--- tree.h
+++ tree.h
@@ -1,15 +1,6 @@
 #ifndef UNTITLED1_LOC_H
 #define UNTITLED1_LOC_H
 #include "list.h"
-typedef struct s_node_n_aire{
-    int value;
-    char* action;
-    t_list_tree* list_son;
-} t_node_n_aire;
-
-t_node_n_aire* create_node_n_aire(int value, char *action);
-
-
 
 
 
