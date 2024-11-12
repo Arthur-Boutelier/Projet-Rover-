413da39d2511ec08b7d17294957067dde1cd6233
benjamintonneau <benjamin.tonneau@efrei.net>
2024-11-12 22:31:15 +0100

    Création des structures t_tree, t_node, t_cell_move et t_list_move

--- list.h
+++ list.h
@@ -1,5 +1,16 @@
 #ifndef UNTITLED1_LIST_H
 #define UNTITLED1_LIST_H
 #include "tree.h"
+#include "moves.h"
+
+typedef struct s_cell_move
+{
+    t_move value;
+    struct s_cell_move * next;
+} t_cell_move;
+
+typedef struct s_list_move{
+    t_cell_move * head;
+}t_list_move;
 
 #endif //UNTITLED1_LIST_H
--- tree.h
+++ tree.h
@@ -2,6 +2,16 @@
 #define UNTITLED1_LOC_H
 #include "list.h"
 
+typedef struct s_node{
+    int value;
+    t_move move;
+    t_list_move available_move;
+    t_list_son son;
+} t_node;
 
+typedef struct s_tree
+{
+    t_node* racine;
+} t_tree;
 
 #endif //UNTITLED1_LOC_H
