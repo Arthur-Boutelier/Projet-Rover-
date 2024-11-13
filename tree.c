6732b00c11c7a1c1fbcd0e9e527574b5bf6dca84
Arthur <arthur.boutelier@gmail.com>
2024-11-13 09:17:19 +0100

    prototype de différente fonction en rapport avec la création de l'arbre et début des fonctions

--- tree.c
+++ tree.c
@@ -1,3 +1,31 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "tree.h"
+
+t_tree * create_empty_tree(){
+    t_tree * new_tree = (t_tree *) malloc(sizeof(t_tree));
+    new_tree->root = NULL;
+    return new_tree;
+}
+
+
+t_node * create_empty_node(){
+    t_node * new_node = (t_node*) malloc(sizeof(t_node));
+    return new_node;
+}
+
+void fill_node(t_node * node, int value, t_move move, t_list_move available_move, t_localisation loc){
+    node->value = value;
+    node->move = move;
+    node->available_move = available_move;
+    node->loc = loc;
+    node->son = NULL;
+}
+
+/*
+void creating_tree(t_tree * tree, t_list_move * available_move, int depht, t_position initial_position){
+    t_node * root = create_empty_node();
+    fill_node(root, d)
+}
+/*void creating_tree_node(t_node * node, int depht);
+t_list_move * find_optimal_move(t_localisation loc, t_list_move available_move); */
--- tree.h
+++ tree.h
@@ -1,17 +1,29 @@
-#ifndef UNTITLED1_LOC_H
-#define UNTITLED1_LOC_H
+
+#ifndef UNTITLED1_TREE_H
+#define UNTITLED1_TREE_H
+#include "loc.h"
 #include "list.h"
+#include "moves.h"
+
 
 typedef struct s_node{
     int value;
     t_move move;
     t_list_move available_move;
-    t_list_son son;
+    t_list_son * son;
+    t_localisation loc;
 } t_node;
 
 typedef struct s_tree
 {
-    t_node* racine;
+    t_node* root;
 } t_tree;
 
-#endif //UNTITLED1_LOC_H
+t_tree * create_empty_tree();
+t_node * create_empty_node();
+void fill_node(t_node * node, int value, t_move move, t_list_move available_move, t_localisation loc);
+void creating_tree(t_tree * tree, t_list_move * available_move, int depht, t_position initial_position);
+void creating_tree_node(t_node * node, int depht);
+t_list_move * find_optimal_move(t_localisation loc, t_list_move available_move);
+
+#endif //UNTITLED1_TREE_H
