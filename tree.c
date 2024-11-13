da86506c82919c83490381255bc7df0014de07d2
Arthur <arthur.boutelier@gmail.com>
2024-11-13 11:35:26 +0100

    continuer les quelques fonctions et modif des structures

--- tree.c
+++ tree.c
@@ -13,21 +13,31 @@ t_tree * create_empty_tree(){
 
 
 t_node * create_empty_node(){
-    t_node * new_node = (t_node*) malloc(sizeof(t_node));
+    t_node * new_node = (t_node*)malloc(sizeof(t_node));
     return new_node;
 }
 
-void fill_node(t_node * node, int value, t_move move, t_list_move available_move, t_localisation loc){
+void fill_node(t_node * node, int value, t_move* move, t_list_move* available_move, t_localisation loc, t_map map){
     node->value = value;
     node->move = move;
     node->available_move = available_move;
     node->loc = loc;
     node->son = NULL;
+    node-> map = map;
+}
+
+t_tree* creating_tree(t_list_move * available_move, int depht, t_localisation initial_position, t_map map){
+    t_tree * tree = create_empty_tree();
+    t_node * root = create_empty_node();
+    fill_node(root, map.costs[initial_position.pos.x][initial_position.pos.y], NULL, available_move, initial_position, map);
+    tree->root = root;
+    creating_tree_node(tree->root, depht);
+    return tree;
 }
 /*
-void creating_tree(t_tree * tree, t_list_move * available_move, int depht, t_position initial_position){
-    t_node * root = create_empty_node();
-    fill_node(root, d)
+t_list_move * find_optimal_move(t_localisation loc, t_list_move * available_move, t_map map){
+    tab_move_opti =
+    int min =
 }
 /*void creating_tree_node(t_node * node, int depht);
-t_list_move * find_optimal_move(t_localisation loc, t_list_move available_move); */
+ */
--- tree.h
+++ tree.h
@@ -4,14 +4,16 @@
 #include "loc.h"
 #include "list.h"
 #include "moves.h"
+#include "map.h"
 
 
 typedef struct s_node{
     int value;
-    t_move move;
-    t_list_move available_move;
+    t_move * move;
+    t_list_move * available_move;
     t_list_son * son;
     t_localisation loc;
+    t_map map;
 } t_node;
 
 typedef struct s_tree
@@ -21,9 +23,9 @@ typedef struct s_tree
 
 t_tree * create_empty_tree();
 t_node * create_empty_node();
-void fill_node(t_node * node, int value, t_move move, t_list_move available_move, t_localisation loc);
-void creating_tree(t_tree * tree, t_list_move * available_move, int depht, t_position initial_position);
+void fill_node(t_node * node, int value, t_move * move, t_list_move* available_move, t_localisation loc, t_map map);
+t_tree * creating_tree(t_list_move * available_move, int depht, t_localisation initial_position, t_map map);
 void creating_tree_node(t_node * node, int depht);
-t_list_move * find_optimal_move(t_localisation loc, t_list_move available_move);
+t_list_move * find_optimal_move(t_localisation loc, t_list_move * available_move, t_map map);
 
 #endif //UNTITLED1_TREE_H
