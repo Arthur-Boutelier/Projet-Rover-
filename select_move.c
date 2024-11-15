6a6d2b3d20aa62cf371887b5171dffda4ded6f7e
vico <victor.proffit@efrei.net>
2024-11-15 19:30:31 +0100

    création set_list_move (a tester)

--- select_move.c
+++ select_move.c
@@ -1,6 +1,15 @@
 #include "select_move.h"
 #include "list.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
+
+int num_random_time()
+{
+  srand(time(NULL));
+  return rand();
+}
 
 
 t_list_freemove* create_t_list_init()
@@ -16,10 +25,17 @@ t_list_freemove* create_t_list_init()
   return list_init;
 }
 
-
-
-
-
-
-
-
+t_list_move* set_list_move(t_list_freemove* list){
+  t_list_move* list_move = create_empty_list_move();
+  for (int i=0; i<9; i++){
+    int number_random = (num_random_time()%(100-i))+1;
+    t_cell_freemove* current = list->head;
+    while(current->numberofmoveleft > number_random)
+    {
+      number_random = number_random - current->numberofmoveleft;
+      current = current->next;
+    }
+    addHead_cell_move(list_move, current->value);
+  }
+  return list_move;
+}
--- select_move.h
+++ select_move.h
@@ -8,7 +8,7 @@
 
 t_list_freemove* create_t_list_init();
 
-t_list_freemove* set_list_move(t_list_freemove* list);
+t_list_move* set_list_move(t_list_freemove* list)
 
 
 #endif //UNTITLED1_SELECT_MOVE_H
